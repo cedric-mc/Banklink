051b0b2a2baca8eff391a358aa706acaea5fc3fd
cedric-mc <cedric.mc11@gmail.com>
2023-12-21 16:59:42 +0100

    21/12/2023 16:59

--- admin/suppr_client.php
+++ admin/suppr_client.php
@@ -3,16 +3,14 @@
 include("../includes/conf.php");
 
 // Récupérer l'identifiant de l'utilisateur à supprimer depuis la variable GET
-if (isset($_GET['id'])) {
-    $idUserToSuppr = $_GET['id'];
+if (!empty($_GET['id'])) {
     // Requête SQL pour mettre à jour la valeur de suppr à 1
     $updateQuery = "UPDATE CLIENT SET suppr = 1 WHERE idUser = :idUser";
     $stmt = $cnx->prepare($updateQuery);
-    $stmt->bindParam(':idUser', $idUserToSuppr, PDO::PARAM_INT);
+    $stmt->bindParam(':idUser', $_GET['id'], PDO::PARAM_INT);
     $stmt->execute();
-
     // Rediriger vers la page précédente tout en affichant un message de succès
-    header("Location: ./?etat=1");
+    header("Location: index.php?etat=1");
 }
 header("Location: ./");
 ?>
